f23c2db7e589192baab4151f8365d16fba9a3ddc
Goldn7799 <syeifsultan@gmail.com>
2023-06-08 22:53:37 +0700

    view update

--- sources/script/main.js
+++ sources/script/main.js
@@ -97,22 +97,135 @@ const page = {
         </nav>
         <div class="itemElips itemElipsClose" id="itemElips">
           <div class="arrowUp"></div>
-          <div id="elipsSelection"></div>
+          <div id="elipsSelection">
+            <button class="btn btn-danger" onClick="logOut()">LogOut</button>
+          </div>
         </div>
+        <div id="chatListRoom"></div>
       </div>
     `
     let elipsSwitch = false
     document.getElementById('elipsBtn').addEventListener('click', ()=>{
       if (elipsSwitch) {
         elipsSwitch = false
+        document.getElementById('elipsBtn').innerHTML = '<i class="fa-solid fa-lg fa-ellipsis-vertical"></i>'
         document.getElementById('itemElips').classList.remove('itemElipsOpen')
         document.getElementById('itemElips').classList.add('itemElipsClose')
       } else {
         elipsSwitch = true
+        document.getElementById('elipsBtn').innerHTML = '<i class="fa-solid fa-lg fa-xmark"></i>'
         document.getElementById('itemElips').classList.add('itemElipsOpen')
         document.getElementById('itemElips').classList.remove('itemElipsClose')
       }
     })
+    const checkLoadedChatMsg = (data) => {
+      return new Promise((resolve)=>{
+        let loadedChatMsg = JSON.parse(data);
+        const allChatId = Object.keys(loadedChatMsg);
+        for (const chatId of credUser.eData.UserState.GroupList) {
+          if (!(allChatId.includes(chatId))) {
+            loadedChatMsg[chatId] = {
+              Name: 'none',
+              LastChat: {},
+              UnRead: []
+            };
+          };
+        }
+        for (const chatId of credUser.eData.UserState.PrivateChat) {
+          if (!(allChatId.includes(chatId))) {
+            loadedChatMsg[chatId] = {
+              Name: 'none',
+              LastChat: {},
+              UnRead: []
+            };
+          };
+        }
+        resolve(loadedChatMsg)
+      })
+    }
+    const convertLoadedToPost = (data)=>{
+      return new Promise((resolve)=>{
+        const allChatId = Object.keys(data);
+        let Result = {}
+        for (const chatId of allChatId) {
+          if ((data[chatId].UnRead).length > 0) {
+            Result[chatId] = (data[chatId].UnRead).map((msg) => {
+              return msg.Id
+            })
+          } else {
+            Result[chatId] = []
+          }
+        }
+        resolve(Result)
+      })
+    }
+    let currentChatList = ''
+    const renderChatList = async (data) => {
+      if (currentChatList !== JSON.stringify(data)) {
+        currentChatList = JSON.stringify(data);
+        const chatListRoom = document.getElementById('chatListRoom');
+        const allChatId = Object.keys(data);
+        let chatListRoomValue = ''
+        for (const chatId of allChatId) {
+          const selected = data[chatId]
+          chatListRoomValue += `
+            <div class="thisChat" id="global">
+              <img src="${(chatId.includes('-')) ? `${firebaseDb.API}/user/profile/${(chatId.split('-')).filter(id => id !== credUser.data.uid)}` : `${firebaseDb.API}/group/profile/${chatId}`}" alt="blank Group">
+              <div class="chatInfo">
+                <h8>${selected.Name}</h8>
+                <p ${(!selected.LastChat) ? 'style="opacity: 0;"' : ''}>${(await getUsername(selected.LastChat?.From))?.DisplayName}: ${(selected.LastChat?.Type === 'text') ? selected.LastChat?.Body : selected.LastChat?.Type}</p>
+              </div>
+              <div class="chatInfo2">
+                <p ${(selected.UnRead.length === 0) ? 'style="opacity: 0;"' : ''}>${selected.UnRead.length}</p>
+                <p ${(!selected.LastChat) ? 'style="opacity: 0;"' : ''}>${selected.LastChat?.Time}</p>
+              </div>
+            </div>
+          `
+        }
+        chatListRoom.innerHTML = chatListRoomValue
+      }
+      setTimeout(() => {
+        loadChatList()
+      }, 2512);
+    }
+    const loadChatList = async ()=>{
+      let loadedChatMsg = localStorage.getItem('loadedChatMsg')
+      if (`${loadedChatMsg}`.length > 10) {
+        loadedChatMsg = await checkLoadedChatMsg(loadedChatMsg)
+      } else {
+        loadedChatMsg = await checkLoadedChatMsg('{}')
+      }
+      fetch(`${firebaseDb.API}/chats/MessageNMinimal/fromUser/${credUser.data.uid}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          loadedChatMsg: await convertLoadedToPost(loadedChatMsg)
+        })
+      })
+      .then((rawRes) => { return rawRes.json() })
+      .then(async (res) => {
+        const { data } = res;
+        const allChatId = Object.keys(data);
+        for (const chatId of allChatId) {
+          const selected = data[chatId];
+          loadedChatMsg[chatId].Name = selected.Name;
+          loadedChatMsg[chatId].LastChat = selected.LastChat;
+          for (const msg of selected.FilteredMsg) {
+            loadedChatMsg[chatId].UnRead.push(msg)
+          }
+        }
+        localStorage.setItem('loadedChatMsg', JSON.stringify(loadedChatMsg))
+        renderChatList(loadedChatMsg)
+      })
+      .catch(()=>{
+        setTimeout(() => {
+          loadChatList()
+        }, 5000);
+      })
+    }
+    loadChatList()
   }
 }
 
@@ -218,6 +331,23 @@ setInterval(() => {
     }
   };
 }, 500);
+
+// Fetch
+const getUsername = (id)=>{
+  return new Promise((resolve)=>{
+    fetch(`${firebaseDb.API}/users/getUsername/${id}`, { method: 'GET' })
+    .then((rawRes) => { return rawRes.json()})
+    .then((res) => {
+      resolve(res.data)
+    })
+    .catch(()=> {
+      resolve({
+        DisplayName: 'Deleted User',
+        UserName: 'deleted_user'
+      })
+    })
+  })
+}
 
 // Fetch Area
 const checkUser = (id) => {
